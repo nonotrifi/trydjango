4d3a5c9d4cff932b59b9208a5e98f27caf07a05b
nono_tr <trifinourdine@outlook.fr>
2022-01-13 12:20:26 +0100

    cinquieme

--- src/trydjango/products/models.py
+++ src/trydjango/products/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Product(models.Model):
-    titre = models.CharField(max_length=120)
+    title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    prix = models.DecimalField(decimal_places=2, max_digits=100)
-    sommaire = models.TextField(blank=False, null=False)
+    price = models.DecimalField(decimal_places=2, max_digits=100)
+    summary = models.TextField(blank=False, null=False)
     featured = models.BooleanField() # null=True, default=True
 # Create your models here.
